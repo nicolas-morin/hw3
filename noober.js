4c2c99330ee48be05af08f53613517e70430a4aa
Nicolas Morin <nicolas.morin@kellogg.northwestern.edu>
2021-01-24 18:39:22 -0600

    Added Passenger Details

--- noober.js
+++ noober.js
@@ -24,19 +24,21 @@ function pageLoad() {
 
   passenger1Name =
     ride[0].passengerDetails.first + " " + ride[0].passengerDetails.last;
-  passenger1Phone = ride[0].passengerDetails.phone;
+  passenger1Phone = ride[0].passengerDetails.phoneNumber;
   passenger1NumberOfPassengers = ride[0].passengerDetails.numberOfPassenger;
-  passenger1PickupAddressLine1 =
-    ride[0].passengerDetails.pickupLocation.address;
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address;
   passenger1PickupAddressLine2 =
-    ride[0].passengerDetails.pickupLocation.city +
+    ride[0].pickupLocation.city +
     ", " +
-    ride[0].passengerDetails.pickupLocation.state +
-    ride[0].passengerDetails.pickupLocation.zip;
-  passenger1DropoffAddressLine1 =
-    ride[0].passengersDetails.dropoffLocation.address;
+    ride[0].pickupLocation.state +
+    ride[0].pickupLocation.zip;
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address;
   passenger1DropoffAddressLine2 =
-    ride[0].passengersDetails.dropoffLocation.city + ", " ride[0].passengerDetails.dropoffLocation.state + " ," ride[0].passengerDetails.dropoffLocation.zip;
+    ride[0].dropoffLocation.city +
+    ", " +
+    ride[0].dropoffLocation.state +
+    " ," +
+    ride[0].dropoffLocation.zip;
 
   console.log(passenger1Name);
   console.log(passenger1Phone);
@@ -46,6 +48,31 @@ function pageLoad() {
   console.log(passenger1DropoffAddressLine1);
   console.log(passenger1DropoffAddressLine2);
 
+  passenger2Name =
+    ride[1].passengerDetails.first + " " + ride[1].passengerDetails.last;
+  passenger2Phone = ride[1].passengerDetails.phoneNumber;
+  passenger2NumberOfPassengers = ride[1].passengerDetails.numberOfPassenger;
+  passenger2PickupAddressLine1 = ride[1].pickupLocation.address;
+  passenger2PickupAddressLine2 =
+    ride[1].pickupLocation.city +
+    ", " +
+    ride[1].pickupLocation.state +
+    ride[1].pickupLocation.zip;
+  passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address;
+  passenger2DropoffAddressLine2 =
+    ride[1].dropoffLocation.city +
+    ", " +
+    ride[1].dropoffLocation.state +
+    " ," +
+    ride[1].dropoffLocation.zip;
+
+  console.log(passenger2Name);
+  console.log(passenger2Phone);
+  console.log(passenger2NumberOfPassengers);
+  console.log(passenger2PickupAddressLine1);
+  console.log(passenger2PickupAddressLine2);
+  console.log(passenger2DropoffAddressLine1);
+  console.log(passenger2DropoffAddressLine2);
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
