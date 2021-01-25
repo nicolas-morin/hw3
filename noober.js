b531eaaa6139e8728f53bf7135f929fc83e9a7ff
Nicolas Morin <nicolas.morin@kellogg.northwestern.edu>
2021-01-24 18:16:06 -0600

    Added Service Code

--- noober.js
+++ noober.js
@@ -6,7 +6,45 @@ function pageLoad() {
   // is refreshed or reloaded, the 'ride' variable will contain something different
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
-  console.log(ride)
+  console.log(ride);
+
+  numberOfPassengers = rideData.length;
+
+  if (ride.length > 1 || ride[0].numberOfPassengers == 1) {
+    levelOfService = "Noober Pool";
+  } else if ((ride.purplerequested = true)) {
+    levelOfService = "Noober Purple";
+  } else if (ride.numberOfPassenger > 3) {
+    levelOfService = "Noober XL";
+  } else {
+    levelOfService = "Noober X";
+  }
+
+  console.log(levelOfService);
+
+  passenger1Name =
+    ride[0].passengerDetails.first + " " + ride[0].passengerDetails.last;
+  passenger1Phone = ride[0].passengerDetails.phone;
+  passenger1NumberOfPassengers = ride[0].passengerDetails.numberOfPassenger;
+  passenger1PickupAddressLine1 =
+    ride[0].passengerDetails.pickupLocation.address;
+  passenger1PickupAddressLine2 =
+    ride[0].passengerDetails.pickupLocation.city +
+    ", " +
+    ride[0].passengerDetails.pickupLocation.state +
+    ride[0].passengerDetails.pickupLocation.zip;
+  passenger1DropoffAddressLine1 =
+    ride[0].passengersDetails.dropoffLocation.address;
+  passenger1DropoffAddressLine2 =
+    ride[0].passengersDetails.dropoffLocation.city + ", " ride[0].passengerDetails.dropoffLocation.state + " ," ride[0].passengerDetails.dropoffLocation.zip;
+
+  console.log(passenger1Name);
+  console.log(passenger1Phone);
+  console.log(passenger1NumberOfPassengers);
+  console.log(passenger1PickupAddressLine1);
+  console.log(passenger1PickupAddressLine2);
+  console.log(passenger1DropoffAddressLine1);
+  console.log(passenger1DropoffAddressLine2);
 
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
@@ -21,6 +59,6 @@ function pageLoad() {
   // passenger3Name, passenger3Phone, passenger3NumberOfPassengers
   // passenger3PickupAddressLine1, passenger3PickupAddressLine2
   // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
-  
+
   //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 }
